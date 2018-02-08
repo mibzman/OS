02a8acf1f0a431740cfe98a086bc44642895e520
mibzman <you@example.com>
2018-02-08 15:49:05 -0500

    removed extra stuff in script

--- lab1/compileOS.sh
+++ lab1/compileOS.sh
@@ -10,19 +10,14 @@ rm printer.out
 #!/bin/bash -e
 set -e
 
-main () {
-readonly a=(1 2)
-    nasm bootload.asm
-    touch floppya.img
-    dd if=/dev/zero of=floppya.img bs=512 count=2880
-    dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
+nasm bootload.asm
+touch floppya.img
+dd if=/dev/zero of=floppya.img bs=512 count=2880
+dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 
-    bcc -ansi -c -o kernel.o kernel.c
-    as86 kernel.asm -o kernel_asm.o
-    ld86 -o kernel -d kernel.o kernel_asm.o
-    dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
+bcc -ansi -c -o kernel.o kernel.c
+as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel.o kernel_asm.o
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
 
-    bochs -f osxterm.txt
-}
-
-main "$@"
+bochs -f osxterm.txt
