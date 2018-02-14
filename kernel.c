7cd991711bcb4d4e5d39f9c829485252e374e465
mibzman <you@example.com>
2018-02-14 10:54:03 -0500

    fixed extra chars in numbers, added madlibs

--- kernel.c
+++ kernel.c
@@ -30,7 +30,9 @@ void printChar(char*);
 void readString(char[80]);
 void readInt(int* n);
 void printLogo();
-void printInt(int input);
+void printInt(int input, int mode);
+
+void playMadLibs();
 
 int mod(int a, int b);
 int div(int a, int b);
@@ -42,27 +44,50 @@ void main()
 
    makeInterrupt21();
    printLogo();
-   // interrupt(33,0,"Hello world from Sam Borick\r\n\0",1,0);
-   printString("Please enter a command: \r\n\0" , 0);
-   readString(input);
-   printString("You wrote: \0" , 0);
-   printString(input, 0);
-   printString("\r\n\0", 0);
 
-
-   printString("Please enter the number 4: \r\n\0" , 0);
-   readInt(&resultNumber);
-   printInt(resultNumber);
-
-
-   // if (resultNumber == 4){
-   //    printString("You wrote 4! good! \r\n\0" , 0);   
-   // } else {
-   //    printString("You did not write 4! bad! \r\n\0" , 0);   
-   // }
+   playMadLibs();
    
-
    while(1);
+}
+
+void playMadLibs(){
+   char food[25], adjective[25], color[25], animal[25];
+   int temp;  
+
+   interrupt(33,0,"\r\nWelcome to the Mad Libs kernel.\r\n\0",0,0);
+   interrupt(33,0,"Enter a food: \0",0,0);
+   interrupt(33,1,food,0,0);
+   temp = 0;
+   while ((temp < 100) || (temp > 120)) {
+      interrupt(33,0,"Enter a number between 100 and 120: \0",0,0);
+      interrupt(33,14,&temp,0,0);
+   }
+   interrupt(33,0,"Enter an adjective: \0",0,0);
+   interrupt(33,1,adjective,0,0);
+   interrupt(33,0,"Enter a color: \0",0,0);
+   interrupt(33,1,color,0,0);
+   interrupt(33,0,"Enter an animal: \0",0,0);
+   interrupt(33,1,animal,0,0);
+   interrupt(33,0,"Your note is on the printer, go get it.\r\n\0",0,0);
+   interrupt(33,0,"Dear Professor O\'Neil,\r\n\0",1,0);
+   interrupt(33,0,"\r\nI am so sorry that I am unable to turn in my program at this time.\r\n\0",1,0);
+   interrupt(33,0,"First, I ate a rotten \0",1,0);
+   interrupt(33,0,food,1,0);
+   interrupt(33,0,", which made me turn \0",1,0);
+   interrupt(33,0,color,1,0);
+   interrupt(33,0," and extremely ill.\r\n\0",1,0);
+   interrupt(33,0,"I came down with a fever of \0",1,0);
+   interrupt(33,13,temp,1,0);
+   interrupt(33,0,". Next my \0",1,0);
+   interrupt(33,0,adjective,1,0);
+   interrupt(33,0," pet \0",1,0);
+   interrupt(33,0,animal,1,0);
+   interrupt(33,0," must have\r\nsmelled the remains of the \0",1,0);
+   interrupt(33,0,food,1,0);
+   interrupt(33,0," on my computer, because he ate it. I am\r\n\0",1,0);
+   interrupt(33,0,"currently rewriting the program and hope you will accept it late.\r\n\0",1,0);
+   interrupt(33,0,"\r\nSincerely,\r\n\0",1,0);
+   interrupt(33,0,"Sam Borick\r\n\0",1,0);
 }
 
 void printString(char* c, int d)
@@ -90,7 +115,7 @@ void printChar(char* c)
    return;
 }
 
-void printInt(int input) {
+void printInt(int input, int mode) {
    char result[80];
    int counter = 0;
    int temp;
@@ -110,13 +135,15 @@ void printInt(int input) {
    for(temp=input; temp > 0; temp = div(temp, 10), counter++){
       result[counter] = '\0';
    }
+   result[counter] = '\0';
+   result[counter+1] = '\0';
    //now our counter is at the 'end' of our string
 
    for (temp=input; temp > 0; temp = div(temp, 10)){
       result[--counter] = mod(temp, 10) + '0';
    }
 
-   printString(result, 0);
+   printString(result, mode);
 }
 
 void printLogo()
@@ -185,7 +212,8 @@ void handleInterrupt21(int ax, int bx, int cx, int dx)
 {
    switch(ax) {  
       case 0: printString(bx,cx); break;
-      case 13: printInt(bx); break;
+      case 1: readString(bx); break;
+      case 13: printInt(bx,cx); break;
       case 14: readInt(bx); break;
       default: printString("General BlackDOS error.\r\n\0"); 
    }  
