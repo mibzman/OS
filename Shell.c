afc5d3e558fced08231da712e67dc8e18b266088
mibzman <you@example.com>
2018-04-06 10:59:57 -0400

    echoing shell

--- Shell.c
+++ Shell.c
@@ -1,3 +1,12 @@
 void main(){
+	char input[25];
 
+	interrupt(33, 0, "blackdos ~(__^>\0", 0, 0);
+	interrupt(33, 1, input, 0, 0);
+
+	interrupt(33, 0, "\r\nYou entered: \0", 0, 0);
+	interrupt(33, 0, input, 0, 0);
+	interrupt(33, 0, "\r\n\0", 0, 0);
+
+	// while (1) ;
 } 
