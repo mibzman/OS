889e33b1f63f6d4789a294aca9dc0a96ea2e7188
mibzman <you@example.com>
2018-04-06 10:43:19 -0400

    Added shell to build

--- compileOS.sh
+++ compileOS.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git rm --cached `git ls-files -i --exclude-from=.gitignore`
+# git rm --cached `git ls-files -i --exclude-from=.gitignore`
 
 #!/bin/bash -e
 set -e
@@ -22,17 +22,22 @@ dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
 dd if=map of=floppya.img bs=512 count=1 seek=256 conv=notrunc
 dd if=config of=floppya.img bs=512 count=1 seek=258 conv=notrunc
 
+#compile kernel c and stick it on the floppy
 bcc -ansi -c -o kernel.o kernel.c
 as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel.o kernel_asm.o
 dd if=kernel of=floppya.img bs=512 conv=notrunc seek=259
 dd if=msg of=floppya.img bs=512 count=1 seek=30 conv=notrunc
 
+bcc -ansi -c -o Shell.o Shell.c
+as86 blackdos.asm -o Shell_asm.o
+ld86 -o Shell -d Shell.o Shell_asm.o
+
 # ./loadFile spc02
 # ./loadFile kitty1
 # ./loadFile kitty2
-./loadFile fib
+./loadFile Shell
 
 bochs -f osxterm.txt
 
-dd if=floppy.img of=config bs=512 skip=258 count=1 conv=notrunc
+dd if=floppya.img of=config bs=512 skip=258 count=1 conv=notrunc
