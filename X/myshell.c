2a4823c4567c9242ab07f84b7333d37f8c611c3f
mibzman <you@example.com>
2018-04-17 12:19:19 -0400

    didn't need to implement our own copy stuff

--- X/myshell.c
+++ X/myshell.c
@@ -41,7 +41,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <fcntl.h>
+
+// #include <fcntl.h>
 
 // #include <sys/types.h>
 // #include <errno.h>
@@ -66,49 +67,6 @@ int parseCommand(char *, struct command_t *);
 void printPrompt();
 void readCommand(char *);
 
-void copy(char* file1, char* file2) {
-  FILE *stream1, *stream2;
-
-  stream1 = fopen(file1, "r");
-  if(stream1 == NULL) {
-    perror("cp: file1 not found");
-    return;
-  }
-
-  //make the file if it doesn't exist
-  stream2 = fopen(file2, "ab+");
-  fclose(stream2);
-
-  stream2 = fopen(file2,"w+");
-  if(stream2 == NULL) {
-    perror("cp: file2 not found");
-    fclose(stream1);
-    return;
-  }
-
-  if(open(file2, O_WRONLY) < 0 || open(file1, O_RDONLY) < 0)
-  {
-    perror("cp: access denied");
-    return;
-  }
-
-  char temp;
-  while((temp = getc(stream1)) != EOF) {
-    putc(temp, stream2);
-  }
-
-  fclose(stream1);
-  fclose(stream2);
-}
-
-void delete(char* file) {
-  if (remove(file) == 0){
-    printf("Deleted\n");
-  } else {
-    printf("Unable to delete\n");
-  }
-}
-
 int main(int argc, char *argv[]) {
    int pid;
    int status;
@@ -130,14 +88,16 @@ int main(int argc, char *argv[]) {
         char* file2 = command.argv[2];
 
         if(command.argc > 2 && strlen(file1) > 0 && strlen(file2) > 0) {
-          copy(file1, file2);
+          command.name = "cp";
         } else {
           printf("insufficient parameters\n");
         }
       } else if(strcmp(command.name, "D") == 0) {
         char* file = command.argv[1];
         if(command.argc > 1 && strlen(file) > 0) {
-          delete(file);
+
+          command.name = "rm";
+
         } else {
           printf("insufficient parameters\n");
         }
@@ -147,14 +107,15 @@ int main(int argc, char *argv[]) {
       } else if(strcmp(command.name, "H") == 0) {
         printf("C file1 file2 Copy; create file2, copy all bytes of file1 to file2 without deleting file1. \n D file Delete; the named file. \n E comment Echo; display comment on screen followed by a new line\n H Help; display the user manual\n L List; the contents of the current directory; see below. \n M file Make; create the named text file by launching a text editor. \n P file Print; display the contents of the named file on screen. \n Q Quit; the shell. \n S Surf; the web by launching a browser as a background process. \n W Wipe; clear the screen. \n X program Execute; the named program. \n");
       } else {
-        /* Create a child process to execute the command */
-        if ((pid = fork()) == 0) {
-           /* Child executing command */
-           execvp(command.name, command.argv);
-        }
-        /* Wait for the child to terminate */
-        wait(&status);
       }
+
+      /* Create a child process to execute the command */
+      if ((pid = fork()) == 0) {
+         /* Child executing command */
+         execvp(command.name, command.argv);
+      }
+      /* Wait for the child to terminate */
+      wait(&status);
    }
 
    /* Shell termination */
